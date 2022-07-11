3cab6b6b321815f5e8ff8f2972868ddf4cc6ad01
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-11 18:44:42 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,18 @@
+from pyrogram import *
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+generate_single_button = [InlineKeyboardButton("⍟ ꜱᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛᴇ ꜱᴇꜱꜱɪᴏɴ ⍟", callback_data="generate")]
+
+generate_button = [generate_single_button]
+
+
+
+#╔════╗────────╔═══╗
+#║╔╗╔╗║────────║╔══╝
+#╚╝║║╠╩═╦══╦╗╔╗║╚══╦══╦╦══╗
+#──║║║║═╣╔╗║╚╝║║╔══╣╔╗╠╣╔═╝
+#──║║║║═╣╔╗║║║║║╚══╣╚╝║║╚═╗
+#──╚╝╚══╩╝╚╩╩╩╝╚═══╣╔═╩╩══╝
+#──────────────────║║
+#──────────────────╚╝
