b1a1e02dbdccac10c070f2224c1e48046e773238
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-12 13:35:53 +0530

    Create callback.py

---
+++ module/callback.py
@@ -0,0 +1,23 @@
+import traceback
+from script import *
+from module.main import *
+from pyrogram import Client
+from pyrogram.types import CallbackQuery, InlineKeyboardMarkup
+from module.generate import generate_session, ask_ques, buttons_ques
+
+
+@Client.on_callback_query()  
+async def tgm(bot, update):  
+    if update.data == "add": 
+        await update.answer(
+             text="♻️Adding Soon.....",
+        )
+    elif update.data == "gen":
+         await update.message.edit_text(
+             text=ask_ques,
+             reply_markup=buttons_ques,
+             disable_web_page_preview=True
+         )
+         await update.answer(
+             text="🐥 Send Me your Choice 🐥",
+         )
