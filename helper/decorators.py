8095024cca5966236f203f33f0db0ce11f9b7d86
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-12 10:26:05 +0530

    Create decorators.py

---
+++ helper/decorators.py
@@ -0,0 +1,15 @@
+from typing import Callable
+from pyrogram import Client
+from pyrogram.types import Message
+
+def humanbytes(size):
+    """Convert Bytes To Bytes So That Human Can Read It"""
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
