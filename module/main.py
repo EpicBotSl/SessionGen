ff868fdcfac018450849e5ed48e1a9ca1d8fed9d
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-12 10:43:46 +0530

    Create main.py

---
+++ module/main.py
@@ -0,0 +1,57 @@
+import re
+import uuid
+import socket
+import platform
+import os
+import random
+import time
+import math
+import json
+import string
+import traceback
+import psutil
+import asyncio
+import wget
+import motor.motor_asyncio
+import pymongo
+import aiofiles
+import datetime
+from pyrogram.errors.exceptions.bad_request_400 import *
+from pyrogram.errors import *
+from pyrogram import Client, filters
+from pyrogram.errors import *
+from pyrogram.types import *
+from helper.decorators import humanbytes
+from config import *
+from database.db import Database
+from asyncio import *
+import heroku3
+import requests
+from helper.heroku_helper import HerokuHelper
+from helper.fsub import forcesub
+#--------------------------------------------------Db-------------------------------------------------#
+
+
+async def send_msg(user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+        return 200, None
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return send_msg(user_id, message)
+    except InputUserDeactivated:
+        return 400, f"{user_id} : deactivated\n"
+    except UserIsBlocked:
+        return 400, f"{user_id} : user is blocked\n"
+    except PeerIdInvalid:
+        return 400, f"{user_id} : user id invalid\n"
+    except Exception as e:
+        return 500, f"{user_id} : {traceback.format_exc()}\n"
+        
+#------------------------------Db End---------------------------------------------------------#       
+        
+Heroku = heroku3.from_key(HEROKU_API_KEY)
+heroku_api = "https://api.heroku.com"
+
+DATABASE_URL=MONGO_URI
+db = Database(DATABASE_URL, "Memehub_bot")
