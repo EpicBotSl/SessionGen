884e2babf53f83e2e958b6854e18b3365f93c7a0
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-12 11:43:01 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,23 @@
 from pyrogram import *
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors.exceptions.bad_request_400 import *
+from pyrogram.errors import *
+from pyrogram import Client, filters
+from pyrogram.errors import *
+from pyrogram.types import *
 
+FORCESUB_BUTTONS = InlineKeyboardMarkup([[
+                 InlineKeyboardButton('🍁ꜱᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛᴇ ꜱᴇꜱꜱɪᴏᴏɴɴ🍁', callback_data="generate")
+                 ],
+                 [
+                 InlineKeyboardButton('☔ ʜᴇʟᴘ ☔', callback_data="Help"),
+                 InlineKeyboardButton(text="🎋 ᴀʙᴏᴜᴛ 🎋", callback_data="about")
+                 ],
+                 [
+                 InlineKeyboardButton(text="</ᴇᴘɪᴄ ᴅᴇᴠᴇʟᴏᴘᴇʀꜱ</>🇱🇰", url="https://t.me/EpicBotsSl")
+                 ]])
 
-generate_single_button = [InlineKeyboardButton("⍟ ꜱᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛᴇ ꜱᴇꜱꜱɪᴏɴ ⍟", callback_data="generate")]
+generate_single_button = [InlineKeyboardButton("🍁ꜱᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛᴇ ꜱᴇꜱꜱɪᴏɴ🍁", callback_data="generate")]
 
 generate_button = [generate_single_button]
 
